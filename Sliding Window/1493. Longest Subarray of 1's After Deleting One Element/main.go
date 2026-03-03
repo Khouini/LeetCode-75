2bedb89bfb91a2de9a111d8a29d893895ec8bbac
Khouini Yacine <khouini.medyacine@gmail.com>
2026-03-03 16:23:37 +0100

    Refactor code structure for improved readability and maintainability

---
+++ Sliding Window/1493. Longest Subarray of 1's After Deleting One Element/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func longestSubarray(nums []int) int {
+	n := len(nums)
+	L := 0
+	lastZeroIndex := -1
+
+	max := 0
+	for R := 0; R < n; R++ {
+		if nums[R] == 1 {
+			size := R - L
+			if lastZeroIndex == -1 {
+				size = size + 1
+			}
+			if size > max {
+				max = size
+			}
+		}
+
+		if nums[R] == 0 {
+			if lastZeroIndex != -1 {
+				L = lastZeroIndex + 1
+			}
+			lastZeroIndex = R
+		}
+	}
+
+	if lastZeroIndex == -1 {
+		return n - 1
+	}
+	return max
+}
+
+func main() {
+	fmt.Printf("got %d, expected %d\n", longestSubarray([]int{1, 1, 0, 1}), 3)
+	fmt.Printf("got %d, expected %d\n", longestSubarray([]int{0, 1, 1, 1, 0, 1, 1, 0, 1}), 5)
+	fmt.Printf("got %d, expected %d\n", longestSubarray([]int{1, 1, 1}), 2)
+	fmt.Printf("got %d, expected %d\n", longestSubarray([]int{1, 0, 0, 0, 0}), 1)
+	fmt.Printf("got %d, expected %d\n", longestSubarray([]int{1}), 0)
+	fmt.Printf("got %d, expected %d\n", longestSubarray([]int{1, 1, 0, 0}), 2)
+}

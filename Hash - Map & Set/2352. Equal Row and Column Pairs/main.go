32c648610d48401ef64abd30ee9ba727571f38eb
Khouini Yacine <khouini.medyacine@gmail.com>
2026-03-07 00:26:38 +0100

    Refactor equalPairs function to use strings.Builder for improved performance and readability

--- Hash - Map & Set/2352. Equal Row and Column Pairs/main.go
+++ Hash - Map & Set/2352. Equal Row and Column Pairs/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-func equalPairs(grid [][]int) int {
+/*func equalPairs(grid [][]int) int {
 	rows := make([]string, 0)
 	cols := make(map[string]int)
 
@@ -30,6 +31,36 @@ func equalPairs(grid [][]int) int {
 	for _, row := range rows {
 		if _, exists := cols[row]; exists {
 			count = count + cols[row]
+		}
+	}
+
+	return count
+}*/
+
+func equalPairs(grid [][]int) int {
+	rows := make([]string, 0)
+	cols := make(map[string]int)
+
+	n := len(grid)
+
+	for i := 0; i < n; i++ {
+		var rowBuilder, colBuilder strings.Builder
+		for j := 0; j < n; j++ {
+			if j > 0 {
+				rowBuilder.WriteByte(',')
+				colBuilder.WriteByte(',')
+			}
+			rowBuilder.WriteString(strconv.Itoa(grid[i][j]))
+			colBuilder.WriteString(strconv.Itoa(grid[j][i]))
+		}
+		rows = append(rows, rowBuilder.String())
+		cols[colBuilder.String()]++
+	}
+
+	count := 0
+	for _, row := range rows {
+		if _, exists := cols[row]; exists {
+			count += cols[row]
 		}
 	}
 
